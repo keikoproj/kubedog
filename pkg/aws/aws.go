f3b2a2b2fd1de8645b731d193c1c8ed2c71dd6e5
Alfredo Garo <44888596+garomonegro@users.noreply.github.com>
2023-05-19 13:07:25 -0400

    adding license

--- pkg/aws/aws.go
+++ pkg/aws/aws.go
@@ -12,7 +12,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package aws provides steps implementations related to AWS.
 package aws
 
 import (
--- pkg/kube/structured/structured.go
+++ pkg/kube/structured/structured.go
@@ -12,7 +12,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package kube provides steps implementations related to Kubernetes.
 package structured
 
 import (
