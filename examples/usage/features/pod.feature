062eae2b8bfb8190b6ec2bed723d083b91fd6b40
Alfredo Garo <44888596+garomonegro@users.noreply.github.com>
2023-05-19 21:33:56 -0400

    usage example using NO templating

--- examples/usage/features/pod.feature
+++ examples/usage/features/pod.feature
@@ -5,9 +5,9 @@ Feature: Successfully deploying a Kubernetes Pod
     And a Kubernetes cluster
 
   Scenario: Create Namespace and Pod, validate successfull deployment
-    Then create resource generated_namespace.yaml
+    Then create resource namespace.yaml
     And store current time as pod-creation
-    And create resource generated_pod.yaml
-    Then resource generated_pod.yaml should be created
-    And resource generated_pod.yaml condition Ready should be True
+    And create resource pod.yaml
+    Then resource pod.yaml should be created
+    And resource pod.yaml condition Ready should be True
     Then all pods in namespace kubedog-example with selector tier=backend have "Hello, Kubedog!" in logs since pod-creation time
--- examples/usage/main_test.go
+++ examples/usage/main_test.go
@@ -2,38 +2,15 @@ package main
 
 import (
 	"log"
-	"os"
 	"testing"
 
 	"github.com/cucumber/godog"
 	"github.com/keikoproj/kubedog"
-	"github.com/keikoproj/kubedog/examples/usage/generate"
 )
 
 var k kubedog.Test
 
-func InitializeScenario(ctx *godog.ScenarioContext) {
-	k.SetScenario(ctx)
-}
-
-func InitializeTestSuite(ctx *godog.TestSuiteContext) {
-	ctx.BeforeSuite(func() {
-		if err := k.KubeClientSet.DeleteAllTestResources(); err != nil {
-			log.Printf("Failed deleting the test resources: %v", err)
-		}
-	})
-	ctx.AfterSuite(func() {
-		if err := k.KubeClientSet.DeleteAllTestResources(); err != nil {
-			log.Printf("Failed deleting the test resources: %v", err)
-		}
-	})
-	k.SetTestSuite(ctx)
-}
-
 func TestFeatures(t *testing.T) {
-	if err := generate.Templates(); err != nil {
-		t.Error(err)
-	}
 	status := godog.TestSuite{
 		Name:                 "godogs",
 		TestSuiteInitializer: InitializeTestSuite,
@@ -44,5 +21,25 @@ func TestFeatures(t *testing.T) {
 			TestingT: t,
 		},
 	}.Run()
-	os.Exit(status)
+	if status != 0 {
+		t.Fatal("non-zero status returned, failed to run feature tests")
+	}
+}
+
+func InitializeScenario(ctx *godog.ScenarioContext) {
+	k.SetScenario(ctx)
+}
+
+func InitializeTestSuite(ctx *godog.TestSuiteContext) {
+	ctx.BeforeSuite(func() {
+		if err := k.KubeClientSet.DeleteAllTestResources(); err != nil {
+			log.Printf("Failed deleting the test resources: %v\n\n", err)
+		}
+	})
+	ctx.AfterSuite(func() {
+		if err := k.KubeClientSet.DeleteAllTestResources(); err != nil {
+			log.Printf("Failed deleting the test resources: %v\n\n", err)
+		}
+	})
+	k.SetTestSuite(ctx)
 }
